485cfadbb7f493fc21e27499792a703dd2aa1588
Franklyn Kleiton <franklynkleiton18@gmail.com>
2024-07-30 10:02:05 -0300

    init

---
+++ src/myfirstsimulation.gatling.js
@@ -0,0 +1,17 @@
+import { scenario, simulation, constantUsersPerSec } from "@gatling.io/core";
+import { http } from "@gatling.io/http";
+
+export default simulation((setUp) => {
+  const httpProtocol = http
+    .baseUrl("https://computer-database.gatling.io")
+    .acceptHeader("application/json")
+    .contentTypeHeader("application/json");
+
+  const myScenario = scenario("My scenario").exec(
+    http("Request 1").get("/computers/")
+  );
+
+  setUp(myScenario.injectOpen(constantUsersPerSec(2).during(60))).protocols(
+    httpProtocol
+  );
+});
